400d7770557d88c1aac4e063e821d80d3d1f509d
Danny Painter <dannyjpainter@gmail.com>
2022-01-04 21:59:20 -0500

    updated html symbol

--- components/Scalesection.js
+++ components/Scalesection.js
@@ -7,7 +7,7 @@ const Scalesection = () => {
             <div className="relative py-7 lg:pr-4 2xl:pr-7 mx-auto">
               <h1 className="text-base text-purple-500 comment mb-6">Scale</h1>
               <h2 className="text-5xl mb-6 font-medium text-gray-400">Scale with the <br /><span className="text-gray-50">database experts</span></h2>
-              <p className="text-2xl mb-9 text-gray-300 md:max-w-none lg:max-w-[430px] font-normal leading-tight">Don’t wait until it’s too late to decide to scale your data. Rely on the database that is built on software that has scaled the world's biggest companies. Get ahead of your future scale today.</p>
+              <p className="text-2xl mb-9 text-gray-300 md:max-w-none lg:max-w-[430px] font-normal leading-tight">Don&apos;t wait until it&apos;s too late to decide to scale your data. Rely on the database that is built on software that has scaled the world&apos;s biggest companies. Get ahead of your future scale today.</p>
               <a href="" className='flex items-center text-xl font-semibold text-blue-400'>Set up a database consultation
                 <svg className="inline mt-px ml-3"width="8" height="14" viewBox="0 0 5 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.919312 9.19995L4.15257 5.32004C4.30709 5.13462 4.30709 4.86528 4.15257 4.67986L0.919311 0.799952" stroke="#47B7F8" strokeLinecap="round" strokeLinejoin="round"/></svg>
               </a>
